dc0599e2d109f4ffe174211cbe9eca46d27d03f6
Hamed <hamedkiri24@gmail.com>
2022-11-04 00:43:45 +0100

    two

--- functions.py
+++ functions.py
@@ -1,41 +1,52 @@
 import cv2
 import numpy as np
-from tkinter import *
-import pandas as pd
+
+INDEX_AKAZE = 0
+INDEX_BRISK = 1
+INDEX_ORB = 2
+INDEX_SIFT = 3
 
 
 def nothing(x):
     pass
 
 
-def create_detector(nombre_point, index, octave, octave_echelle):  # ,octave
-    if index == 0:
+def create_detector(index):
+
+    """To select different algorithm of features-matching."""
+
+    if index == INDEX_AKAZE:
         # default threshold=0.00001, nOctaves=16
         return cv2.AKAZE_create()
-    elif index == 1:
+    elif index == INDEX_BRISK:
         return cv2.BRISK_create()
-    elif index == 2:
+    elif index == INDEX_ORB:
         # default nfeatures=10000, scaleFactor=1.2
         return cv2.ORB_create()
-    elif index == 3:
+    elif index == INDEX_SIFT:
         return cv2.SIFT_create()
 
 
-def get_features(img, index, nombre_point, octave, octave_echelle):  # ,octave
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    detector = create_detector(index, nombre_point, octave, octave_echelle)  # ,octave
-    kps, descs = detector.detectAndCompute(gray, None)
+def get_features(img, index):
+
+    """Extraction of key points and descriptors with black-white image."""
+
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    detector = create_detector(index=index)
+    kps, descs = detector.detectAndCompute(gray_image, None)
     return kps, descs, img.shape[:2][::-1]
 
 
 
 
 
-def detect_features(img, train_features, index, ratio_test, nombre_point, octave, octave_echelle):  # ,octave
+def matching_features(img, train_features, index, ratio_test):
+
+    """Matching descriptors between images."""
+
     train_kps, train_descs, shape = train_features
     # get features from input image
-    kps, descs, _ = get_features(img, index, nombre_point, octave, octave_echelle)  # ,octave=octave
-    # kps,descs=selectionneur_(img,liste_image_train,1,index,nombre_point,octave,octave_echelle)
+    kps, descs, _ = get_features(img=img, index=index)  # ,octave=octave , octave=octave, octave_echelle=octave_echelle
     # check if keypoints are extracted
     if not kps:
         print("No keypoints extracted")
@@ -44,11 +55,18 @@ def detect_features(img, train_features, index, ratio_test, nombre_point, octave
     # now we need to find matching keypoints in 2 sets of descriptors (from sample image and from current image)
     # knnMatch uses k-nearest neighbours algorithm for that
     # bf = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_SL2)
-    if index == 0:
+    if index == INDEX_AKAZE:
         bf = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_SL2)
-    elif index == 1 or index == 2:
+    elif index == INDEX_BRISK or index == INDEX_ORB:
         bf = cv2.BFMatcher(cv2.NORM_HAMMING2)
-    matches = bf.knnMatch(train_descs, descs, k=2)
+    elif index == INDEX_SIFT:
+        bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+
+    if index != INDEX_SIFT:
+        matches = bf.knnMatch(train_descs, descs, k=2)
+    else:
+        matches = bf.match(train_descs, descs)
+
     # bf = cv2.BFMatcher(cv2.NORM_HAMMING)
     # matches = bf.knnMatch(train_descs, descs, k=2)
 
@@ -78,9 +96,8 @@ def detect_features(img, train_features, index, ratio_test, nombre_point, octave
         m, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
         if m is not None:
             # apply perspective transform to train image corners to get a bounding box coordinates on a sample image
-            scene_points = cv2.perspectiveTransform(
-                np.float32([(0, 0), (0, shape[0] - 1), (shape[1] - 1, shape[0] - 1), (shape[1] - 1, 0)]).reshape(-1, 1,
-                                                                                                                 2), m)
+            scene_points = cv2.perspectiveTransform(np.float32([(0, 0), (0, shape[0] - 1), (shape[1] - 1,
+                                                    shape[0] - 1), (shape[1] - 1, 0)]).reshape(-1, 1, 2), m)
             rect = cv2.minAreaRect(scene_points)
 
             # check resulting rect ratio knowing we have almost square train image
@@ -96,7 +113,6 @@ def detect_features(img, train_features, index, ratio_test, nombre_point, octave
 
 
 
-# -----------------------------------------------------------------------------------------------2
 
 
 
--- view.py
+++ view.py
@@ -1,8 +1,19 @@
 import cv2
 import numpy as np
 from tkinter import *
-from functions import nothing, get_features, detect_features
+from functions import nothing, get_features, matching_features
+from functions import INDEX_AKAZE, INDEX_BRISK, INDEX_ORB, INDEX_SIFT
 
+NAME_ALGORITHM1 = "AKAZE"
+NAME_ALGORITHM2 = "BRISK"
+NAME_ALGORITHM3 = "ORB"
+NAME_ALGORITHM4 = "SIFT"
+NAMES_ALGORITHMS = [0,1,2,3]
+NAMES_ALGORITHMS[INDEX_AKAZE] = NAME_ALGORITHM1
+NAMES_ALGORITHMS[INDEX_BRISK] = NAME_ALGORITHM2
+NAMES_ALGORITHMS[INDEX_ORB] = NAME_ALGORITHM3
+NAMES_ALGORITHMS[INDEX_SIFT] = NAME_ALGORITHM4
+URL_IMAGE = "./images/sable.jpg"
 
 def view():
     def test_algo(event):
@@ -15,16 +26,21 @@ def view():
         cv2.createTrackbar("logo-blur", "Trackbar", 0, 5, nothing)
         cv2.createTrackbar("logo-scale", "Trackbar", 100, 100, nothing)
         cv2.createTrackbar("webcam-blur", "Trackbar", 0, 5, nothing)
-        cv2.createTrackbar("ratio-test", "Trackbar", 80, 100, nothing)
-        cv2.createTrackbar("AKAZE_seuil", "Trackbar", 1, 2, nothing)
-        cv2.createTrackbar("AKAZE_octave_echelle", "Trackbar", 8, 32, nothing)
-        cv2.createTrackbar("AKAZE_octave", "Trackbar", 8, 32, nothing)
+
+        if index != 0:
+            cv2.createTrackbar("ratio-test", "Trackbar", 80, 100, nothing)
+            #points_numbers = cv2.getTrackbarPos("AKAZE_seuil", "Trackbar")
+            #octave = cv2.getTrackbarPos("AKAZE_octave", "Trackbar")
+            #octave_echelle = cv2.getTrackbarPos("AKAZE_octave_echelle", "Trackbar")
+        else:
+            cv2.createTrackbar("AKAZE_seuil", "Trackbar", 1, 2, nothing)
+        #cv2.createTrackbar("AKAZE_octave_echelle", "Trackbar", 8, 32, nothing)
+        #cv2.createTrackbar("AKAZE_octave", "Trackbar", 8, 32, nothing)
         while True:
             # get train features
 
-            img = cv2.imread("./images/hamed.jpg")
+            img = cv2.imread(URL_IMAGE)
             # img = cv2.imread("dessin.png")
-            img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
             # img = cv2.imread("dessin.png", cv2.IMREAD_UNCHANGED)
             # img = img[:,:,:3]
 
@@ -39,23 +55,22 @@ def view():
 
             cv2.imshow('Logo', img)
             ratio_test = cv2.getTrackbarPos("ratio-test", "Trackbar") / 100
-            nombre_point = cv2.getTrackbarPos("AKAZE_seuil", "Trackbar")
-            octave = cv2.getTrackbarPos("AKAZE_octave", "Trackbar")
-            octave_echelle = cv2.getTrackbarPos("AKAZE_octave_echelle", "Trackbar")
+
             print("Getting features from train image ...")
-            train_features = get_features(img, index, nombre_point, octave, octave_echelle)  # ,octave=octave
+            #gray_img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
+
+            train_features = get_features(img=img, index=index)  #  octave=octave, octave_echelle=octave_echelle
             print("train features:", len(train_features[0]))
 
             _, frame = cap.read()
             webcam_blur_intensity = cv2.getTrackbarPos("webcam-blur", "Trackbar")
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
             frame = cv2.GaussianBlur(frame, (webcam_blur_intensity * 2 + 1, webcam_blur_intensity * 2 + 1), 0)
+            #gray_frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+
 
             # detect features on test image
             print("Detecting features in test image ...")
-            region, kps = detect_features(frame, train_features, index, ratio_test=ratio_test,
-                                          nombre_point=nombre_point, octave=octave,
-                                          octave_echelle=octave_echelle)  # ,octave=octave
+            region, kps = matching_features(frame, train_features, index, ratio_test=ratio_test)  # ,octave=octave
             frame = cv2.drawKeypoints(frame, kps, np.array([]), color=(255, 0, 0))
             if region is not None:
                 print("Logo detected ...")
@@ -66,13 +81,13 @@ def view():
             else:
                 print("No logo detected ...")
             if index == 0:
-                cv2.imshow('AKAZE', frame)
+                cv2.imshow(NAME_ALGORITHM1, frame)
             elif index == 1:
-                cv2.imshow('BRISK', frame)
+                cv2.imshow(NAME_ALGORITHM2, frame)
             elif index == 2:
-                cv2.imshow('ORB', frame)
+                cv2.imshow(NAME_ALGORITHM3, frame)
             elif index == 3:
-                cv2.imshow('SIFT', frame)
+                cv2.imshow(NAME_ALGORITHM4, frame)
 
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -80,11 +95,12 @@ def view():
         cv2.destroyAllWindows()
 
     fenetre = Tk()
-    Nom = ["AKAZE", "BRISK", "ORB"]
+
+
     lbox = Listbox(fenetre, width=8, height=3, font="Verdana 30 bold", selectbackground="red")
     lbox.pack(padx=50, pady=51)
 
-    for item in Nom:
+    for item in NAMES_ALGORITHMS:
         lbox.insert(END, item)
 
     lbox.bind("<<ListboxSelect>>", test_algo)
